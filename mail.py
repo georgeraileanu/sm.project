ad4e49a781262326da0c3344ac58325421410fb2
georgeraileanu <george.raileanu@student.tuiasi.ro>
2020-05-15 20:52:46 +0300

    mail code

---
+++ mail.py
@@ -0,0 +1,32 @@
+import smtplib
+from datetime import datetime
+
+smtpUser='test.gyro.sm@gmail.com'
+smtpPass='testac123'
+
+toAdd='test.gyro.sm@gmail.com'
+fromAdd=smtpUser
+
+now = datetime.now()
+dt_string = now.strftime("%d-%b-%Y (%H:%M:%S)")
+#print dt_string
+
+msg="The alarm on your motorcycle has been activated at "
+
+subject= msg + dt_string
+
+header='To: ' + toAdd + '\n' + 'From: ' + fromAdd + '\n' + 'Subject: ' + subject
+
+body= ' From within a Python script'
+
+print header + '\n' + body
+
+server=smtplib.SMTP('smtp.gmail.com', 587)
+
+server.ehlo()
+server.starttls()
+server.ehlo()
+
+server.login(smtpUser,smtpPass)
+server.sendmail(fromAdd,toAdd, header + '\n' + body)
+server.quit()
